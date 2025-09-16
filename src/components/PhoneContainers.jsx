870f39b084143585190fde6f4391a9c5493b525a
SHANTO <rabbymd55555@gmail.com>
2025-09-16 15:04:07 +0600

    added floder structure and navbar and footer

---
+++ src/components/PhoneContainers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhoneContainers = () => {
+    return (
+        <div>
+            <h1>PhoneContainers</h1>
+        </div>
+    );
+};
+
+export default PhoneContainers;
