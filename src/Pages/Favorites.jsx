514028f1354cfad7a871fd4f537429c56ba5474b
SHANTO <rabbymd55555@gmail.com>
2025-09-21 09:24:45 +0600

    added local stroage and remove

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PhoneCard from "../components/PhoneCard";
 import { getFavorites, removeFavorite } from "../utils";
+import EmptyState from "../components/ui/EmptyState";
 
 const Favorites = () => {
   const [displayPhones, setDisplayPhones] = useState([]);
@@ -10,16 +11,22 @@ const Favorites = () => {
     setDisplayPhones(savedPhones);
   }, []);
 
-
-  const handleDelete =(id)=>{
+  const handleDelete = (id) => {
     removeFavorite(id);
-    setDisplayPhones(getFavorites())
-  }
+    setDisplayPhones(getFavorites());
+  };
+
+  if (displayPhones.length < 1) return <EmptyState></EmptyState>;
   return (
     <div className="py-12 ">
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8 mb-8">
         {displayPhones.map((phone) => (
-          <PhoneCard key={phone.id} phone={phone} handleDelete={handleDelete} deletable={true}></PhoneCard>
+          <PhoneCard
+            key={phone.id}
+            phone={phone}
+            handleDelete={handleDelete}
+            deletable={true}
+          ></PhoneCard>
         ))}
       </div>
     </div>
